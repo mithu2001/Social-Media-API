50403953312283b3addacd107154a373b414a113
mithu2001 <guptamithu2019@gmail.com>
2023-02-02 07:01:35 +0530

    Created Schema of post for User

---
+++ models/Post.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const postSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      require: true,
+    },
+    desc: {
+      type: String,
+      max: 500,
+    },
+    img: {
+      type: String,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Post", postSchema);
