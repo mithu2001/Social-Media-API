20bbd6575ca34f2a6b0667bba1b64c750555bd5f
mithu2001 <guptamithu2019@gmail.com>
2023-02-02 07:00:53 +0530

    Created Schema of User

--- models/user.js
+++ models/user.js
@@ -6,14 +6,12 @@ const userSchema = new Schema({
         type:String,
         required:true,
         min:3,
-        max:20,
-        unique:true
+        max:20
     },
     email:{
         type:String,
         required:true,
-        max:50,
-        unique:true
+        max:50
     },
     password:{
         type:String,
@@ -42,7 +40,23 @@ const userSchema = new Schema({
     isAdmin:{
         type:Boolean,
         default: false
-    }
+    },
+    desc:{
+        type:String,
+        max:50
+    },
+    city:{
+        type:String,
+        max:50
+    },
+    from:{
+        type:String,
+        max:50
+    },
+    relationship:{
+        type:Number,
+        enum:[1,2,3],
+    },
 },
 {
     timestamps:true
